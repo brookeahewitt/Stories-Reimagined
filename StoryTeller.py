316b7a8e117d09d5f24cb1ed35e354a33a3728fd
Sabrina-Lopez <sa568892@ucf.edu>
2024-12-06 11:23:24 -0500

    minor fix to main

--- StoryTeller.py
+++ StoryTeller.py
@@ -6,7 +6,7 @@ from MAD.MADstruct import initialize_chat_history, MAD_loop
 
 # Initialize chat history
 proponent_chat_history, moderator_chat_history, opponent_chat_history, author_chat_history = initialize_chat_history()
-print("Author:", story_intro)
+print("Author:", story_intro_prompt())
 
 
 # Can implement ToT here and get a result to feed into MAD 
